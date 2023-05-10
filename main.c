53b228728da337b0e6a0f5a0176d11eb2e6c6ab6
Odarell35 <leratomawash0@gmail.com>
2023-05-10 12:53:53 -0700

    readline works

--- main.c
+++ main.c
@@ -2,9 +2,16 @@
 
 int main(void)
 {
+	ssize_t n;
+	size_t buf_size;
+	char *buf;
 
-
-	prompt_user();
-	my_getline();
+	buf_size = BUFFER_SIZE;
+	n = 0;
+	buf = malloc(sizeof(char) * buf_size);
+	prompt_user();	
+	n = getline(&buf, &buf_size, stdin);
+	write(STDOUT_FILENO, buf, n);
+	free(buf);
 	return (0);
 }
