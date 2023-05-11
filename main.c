56bed349b876758fd0859b08dfe6de5b9a17e0aa
Odarell35 <leratomawash0@gmail.com>
2023-05-11 11:12:55 -0700

    strtok works, prompt works, getline works as well

--- main.c
+++ main.c
@@ -16,21 +16,16 @@ int main(void)
 	buf_size = BUFFER_SIZE;
 	n = 0;
 	delim = " ";
-/*	char arg[10][100];*/
 	buf = malloc(sizeof(char) * buf_size);
-<<<<<<< HEAD
-=======
 	str = malloc(sizeof (char) * buf_size);
->>>>>>> bc52031cd247b8ea0cf26c9deee15e196aa1ad74
 	prompt_user();
 	n = getline(&buf, &buf_size, stdin);
 	write(STDOUT_FILENO, buf, n);
-	strcpy(str, buf);
+	_strcpy(str, buf);
 	token = strtok(str, delim);
 	i = 0;
 		while (token && i < 10)
 		{
-			/*printf("%s\n",token);*/
 			args[i] = token;
 			token = strtok(NULL, delim);
 			i++;
@@ -38,19 +33,9 @@ int main(void)
 		for (k = 0; k < _strlen(*args); k++)
 		{
 			printf("%s\n", args[k]);
+		/*	write(STDOUT_FILENO, args[k], _strlen(args[k]));*/
 		}
-			/*for (j = 0, i = 0; args[i] != NULL; j++, i++)
-			{
-			write(1, args[i], _strlen(args[i]));
-			}
-			strcpy(arg[i], token);
-			token = strtok(NULL, " ");*/
 		
-		
-		/*for (j = 0; j < i; j++)
-		{
-			write(STDOUT_FILENO, arg[i], _strlen(arg[i]));
-		}*/
 	free(buf);
 	free(token);
 	free(str);
