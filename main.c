bc52031cd247b8ea0cf26c9deee15e196aa1ad74
Odarell35 <leratomawash0@gmail.com>
2023-05-11 06:10:22 -0700

    strtok works

--- main.c
+++ main.c
@@ -3,31 +3,50 @@
 int main(void)
 {
 	ssize_t n;
-	int i;
+	char *str;
+	int i, k;
 	size_t buf_size;
-	char *buf, *token;
+	char *buf, *token, *delim;
+	char *args[10];
+
 
 	buf_size = BUFFER_SIZE;
 	n = 0;
-	char arg[10][100];
+	delim = " ";
+/*	char arg[10][100];*/
 	buf = malloc(sizeof(char) * buf_size);
+	str = malloc(sizeof (char) * buf_size);
 	prompt_user();
 	n = getline(&buf, &buf_size, stdin);
 	write(STDOUT_FILENO, buf, n);
-	token = strtok(buf, " ");
-		i = 0;
-		while (token != NULL && i < 10)
+	strcpy(str, buf);
+	token = strtok(str, delim);
+	i = 0;
+		while (token && i < 10)
 		{
-		strcpy(arg[i],token);
-		i++;
-			token = strtok(NULL, " ");
-		
+			/*printf("%s\n",token);*/
+			args[i] = token;
+			token = strtok(NULL, delim);
+			i++;
 		}
-		for (i = 0; i < 10; i++)
+		for (k = 0; k < _strlen(*args); k++)
+		{
+			printf("%s\n", args[k]);
+		}
+			/*for (j = 0, i = 0; args[i] != NULL; j++, i++)
+			{
+			write(1, args[i], _strlen(args[i]));
+			}
+			strcpy(arg[i], token);
+			token = strtok(NULL, " ");*/
+		
+		
+		/*for (j = 0; j < i; j++)
 		{
 			write(STDOUT_FILENO, arg[i], _strlen(arg[i]));
-		}
+		}*/
 	free(buf);
 	free(token);
+	free(str);
 	return (0);
 }
