032d2a48033121ca6fdcab24057d0fe448d89c61
siphoPrince <mabirimisesipho@gmail.com>
2023-05-23 05:03:24 -0700

    change directory

--- check_builtin.c
+++ check_builtin.c
@@ -66,43 +66,45 @@ int change_dir(char **command)
 
 	if (command[1] == NULL)
 	{
-		/* if no path is given, go to the home directory */
-		chdir(_getenv("HOME"));
-	}
-	if (_strcmp(command[1], "-") == 0)
-	{
-		/* if "-" is given, go to the previous directory */
-		chdir(_getenv("OLDPWD"));
-	}
-
-		/* otherwise, use the given path */
-
-
-	if (chdir(command[1]) == -1)
-	{
-		/* try to change the directory */
-		perror("chdir");
-		return (-1);
-	}
 	
-	/* update the PWD and OLDPWD environment variables */
+		if (chdir(_getenv("HOME")) == -1)
+			perror("chdir");
+	}
+	else if (_strcmp(command[1], "-") == 0)
+	{
+	
+		char *old_pwd = _getenv("OLDPWD");
+		if (old_pwd == NULL)
+		{
+			printf("OLDPWD not set\n");
+			return (-1);
+		}
+
+		if (chdir(old_pwd) == -1)
+			perror("chdir");
+	}
+	else
+	{
+	
+		if (chdir(command[1]) == -1)
+		{
+	
+		perror("chdir");
+		}
+	
+	
         if (getcwd(current_wd, sizeof(current_wd)) == NULL)
-        {
                 perror("getcwd");
-                return (-1);
-        }
+        
 
         if (setenv("OLDPWD", _getenv("PWD"), 1) == -1)
-        {
                 perror("setenv");
-                return (-1);
-        }
 
         if (setenv("PWD", current_wd, 1) == -1)
-        {
                 perror("setenv");
-                return (-1);
-        }
+
+	return (0);
+	}
+
 	return (0);
 }
-
